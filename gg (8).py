f7b690a73a3b2b01e705c6ba36b3f5946d91c0a3
Abbas-Ghazavi <118556497+Abbas-Ghazavi@users.noreply.github.com>
2024-03-31 00:04:33 +0330

    remove cm

--- gg (8).py
+++ gg (8).py
@@ -2,16 +2,14 @@ from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, Key
 import time
 import random
-# new lib maybe
-#bad opt pc memory
-#bad cm!!
+
 x_coordinate_min = 780
 x_coordinate_max = 830
 y_coordinate = 600
-# need new locations
+
 mouse = Controller()
 active = True
-# add timer no need a press key
+
 def on_press(key):
     global active
     if key == Key.esc:
@@ -29,7 +27,7 @@ def on_press(key):
     
 def on_release(key):
     pass
-#opt for while
+
 with Listener(on_press=on_press, on_release=on_release) as listener:
     try:
         while active:
@@ -40,10 +38,7 @@ with Listener(on_press=on_press, on_release=on_release) as listener:
             mouse.click(Button.left, 1)
             
             time.sleep(0.01)
-            #fix bug timer
     except KeyboardInterrupt:
         print("stop")
     finally:
         listener.stop()
-#add gui later 
-        #add file readme
